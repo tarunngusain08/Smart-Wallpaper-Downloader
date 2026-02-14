5a58febd8abdccb4dacf8605210ad87d5c072c20
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-14 19:56:56 +0530

    the app works fine: WallShiftApplication.kt

---
+++ app/src/main/java/com/wallshift/app/WallShiftApplication.kt
@@ -0,0 +1,19 @@
+package com.wallshift.app
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class WallShiftApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
