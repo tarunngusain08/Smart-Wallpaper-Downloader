43c02b96c4934ed536e0f939d21f2d381c9b6221
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-14 19:56:57 +0530

    the app works fine: WallShiftApp.kt

---
+++ app/src/main/java/com/wallshift/app/ui/WallShiftApp.kt
@@ -0,0 +1,9 @@
+package com.wallshift.app.ui
+
+import androidx.compose.runtime.Composable
+import com.wallshift.app.ui.navigation.WallShiftNavHost
+
+@Composable
+fun WallShiftApp() {
+    WallShiftNavHost()
+}
